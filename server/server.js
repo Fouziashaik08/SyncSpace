e4cb80f6a976584c14ab551bb33768ff9b529afa
Harshit Bansal <haannuu123@gmail.com>
2026-09-22 22:18:25 +0530

    Fix production CORS

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://syncspace-theta-three.vercel.app" ,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
@@ -46,8 +46,8 @@ const users = {};
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
+origin: "https://syncspace-theta-three.vercel.app"   ,
+ methods: ["GET", "POST"],
   },
 });
 
